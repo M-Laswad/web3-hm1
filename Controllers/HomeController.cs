4cf06fbcb17ceec1ad97ca18996d1867451718a3
Mohammed Bin Laswad <127851576+M-Laswad@users.noreply.github.com>
2023-05-26 16:33:03 +0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Web3_HW1_reverse.Models;
+
+namespace Web3_HW1_reverse.Controllers;
+
+public class HomeController : Controller
+{
+  
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+  [HttpPost]
+    public IActionResult ReverseName(string name)
+    {
+        if (name == null)
+        {
+            ViewBag.Message = "Please enter a valid name.";
+        }
+        else
+        {
+            string reversedName = "";
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                reversedName += name[i];
+            }
+
+            ViewBag.Message = reversedName;
+        }
+        
+        return View("Index");
+    }
+    
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Web3_HW1_reverse.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
